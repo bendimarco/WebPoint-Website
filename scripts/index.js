0758c4397b5772c36a1c5d142d2bf30fd946a640
agoldstein03 <38170312+agoldstein03@users.noreply.github.com>
2020-10-13 16:15:08 -0400

    Add background form submission

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,27 @@
+function getValue(elements, name) {
+    return encodeURIComponent(elements[name].value)
+}
+
 function submit() {
-    swal({
-        title: "Success!",
-        text: "Your message has been sent.",
-        icon: "success",
-        button: "Awesome!",
-      });
+    let elements = document.querySelector("form").elements;
+    fetch(`https://formsubmit.co/webpointexecutives@gmail.com?name=${getValue(elements, "name-input")}&email=${getValue(elements, "email-input")}&message=${getValue(elements, "message-input")}`, {method: "POST"})
+    .then(response => {
+        if (!response.ok) {
+            throw new Error();
+        } else {
+            swal({
+                title: "Success!",
+                text: "Your message has been sent.",
+                icon: "success",
+                button: "Awesome!",
+            });
+        }
+    }).catch(e => {
+        swal({
+            title: "Failure",
+            text: "Something went wrong. Please try again and email us if the issue persists.",
+            icon: "error",
+            button: "Try again!",
+        });
+    });
 }
