f67427c8abba6ac92707b31374dc9147a45e2ef4
Ben DiMarco <58406490+bendimarco@users.noreply.github.com>
2020-10-16 20:08:17 -0400

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,9 @@
-let form = document.querySelector("form");
-form.addEventListener("submit", submit);
-
-function getValue(elements, name) {
-    return elements[name].value;
-}
-
-function getFormJSON() {
-    let elements = form.elements;
-    return JSON.stringify({
-        name: getValue(elements, "name-input"), 
-        email: getValue(elements, "email-input"), 
-        message: getValue(elements, "message-input")
-    });
-}
-
-function submit(e) {
-    e.preventDefault();
+function submit() {
     swal({
         title: "Success!",
         text: "Your message has been sent.",
         icon: "success",
         button: "Awesome!",
-    });
-    fetch("https://cors-anywhere.herokuapp.com/https://formsubmit.co/ajax/webpointexecutives@gmail.com", {method: "POST", headers: {"Content-Type": "application/json"}, body: getFormJSON()})
-    ;/*.then(response => {
-        if (!response.ok) {
-            throw new Error();
-        } else {
-            swal({
-                title: "Success!",
-                text: "Your message has been sent.",
-                icon: "success",
-                button: "Awesome!",
-            });
-        }
-    }).catch(e => {
-        swal({
-            title: "Failure",
-            text: "Something went wrong. Please try again and email us if the issue persists.",
-            icon: "error",
-            button: "Try again!",
-        });
-    });*/
-    return false;
+        // timer: 3000,
+      });
 }
